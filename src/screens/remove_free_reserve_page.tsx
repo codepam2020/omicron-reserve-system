204016dc76ba030eb72e7bf955b1da50bd33f446
codePam <codepam2020@gmail.com>
2023-03-26 15:34:32 +0900

    u

--- src/screens/remove_free_reserve_page.tsx
+++ src/screens/remove_free_reserve_page.tsx
@@ -116,7 +116,7 @@ export default function RemoveFreeReserve () {
       </Link>
 
       <div className="h-6" />
-      <a hidden={true} onClick={() => { setData(state); }}>.</a>
+      <a onClick={() => { setData(state); }} className='text-xs'>.</a>
 
 
     </div>
